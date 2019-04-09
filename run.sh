dbb63ee12bb7ed700270bafaa71c98340d9eafc8
Terrence E. Gilchrist <47313628+TerrenceGilchrist@users.noreply.github.com>
2019-04-09 04:52:27 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 # !/bin/bash
 
-# compiler		 g++
+# compiler	g++
 # source code	    /src/Groceries.cpp
 # optimizer	      -O2
 # input		        /input/products.csv
 # input		        /input/order_products__prior.csv
-# input folder	  /input/
-# output folder	  /output/
+# input folder	        /input/
+# output folder	        /output/
 
 g++ /src/Groceries.cpp -O2 /input/products.csv /input/order_products__prior.csv /input/ /output/
